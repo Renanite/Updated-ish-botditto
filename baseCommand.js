3ffb404d2693707ecef276ae376fa71a6669683d
Renanite <60981192+Renanite@users.noreply.github.com>
2020-02-19 12:26:42 -0600

    Add files via upload
    Updated the FC Commands/Parameters

---
+++ baseCommand.js
@@ -0,0 +1,15 @@
+exports.run = async (client, message, params) => {
+  
+}
+
+exports.conf = {
+  aliases: [],
+  permLevel: 0,
+  nsfw: false
+}
+
+exports.help = {
+  name: "",
+  description: "",
+  usage: ""
+}
